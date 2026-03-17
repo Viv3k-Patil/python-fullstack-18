ee2f52973c6f7d3343822d1139c8db4ca5ae3021
parthfiske <parthfiske7@gmail.com>
2026-03-17 09:09:28 +0530

    programs

---
+++ parth/d4p.py
@@ -0,0 +1,12 @@
+# Take name and age from user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Print greeting message
+print("Hello", name + ", you are", age, "years old.")
+
+# Take a number from user
+num = int(input("Enter a number: "))
+
+# Print binary value
+print("Binary of", num, "is", bin(num))
---
+++ parth/d5hw.py
@@ -0,0 +1,9 @@
+# Write a program that:
+# Takes two numbers from the user
+# Calculates their sum
+# Displays the result using an f-string
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+num = num1 + num2
+print(f"The sum of {num1} and {num2} is {num}")
