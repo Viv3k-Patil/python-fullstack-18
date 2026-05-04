69fae201c38bbe179a26c319b6b0513088d1713c
onkar5578 <onkarsavanji8@gmail.com>
2026-05-04 16:00:38 +0530

    update code

--- team/Onkar/Chapter_1/even_odd_no.py
+++
@@ -1,8 +0,0 @@
-# Take input from the user
-number = int(input("Please enter a : "))
-
-# Check if the number is even or odd
-if number % 2 == 0:
-    print(number, "is an even number.")
-else:
-    print(number, "is an odd number.")
