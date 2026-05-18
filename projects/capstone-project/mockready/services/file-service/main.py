ced329ed9f32592c4575a3c4f935bd9db1e89a24
prathmesh <patilprathmesh4507@gmail.com>
2026-05-18 20:11:50 +0530

    add new

--- projects/capstone-project/mockready/services/file-service/main.py
+++ projects/capstone-project/mockready/services/file-service/main.py
@@ -1,10 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,UploadFile,File,Form
 
 app=FastAPI()
-
+resumes={}
 
 @app.get("/health")
 def check_health():
     return {
         "msg":"server is running and up"
     }
+    
+@app.get("upload")    
+async def upload(
+    name:str,
+    email:str,
+    file:UploadFile=File(...,example="resume.pdf")
+    ):
+    
+    await content=file.read()
+    resumes[file.filename]={
+       " name":name,
+         "email":email,
+         "filename":file.filename,
+         "file":file
+    }
+    return resumes
