84bdeff4689afa36edfc906adac97816ee0faedb
prathmesh <patilprathmesh4507@gmail.com>
2026-05-19 11:59:57 +0530

    add some files

--- projects/capstone-project/mockready/services/file-service/main.py
+++ projects/capstone-project/mockready/services/file-service/main.py
@@ -1,28 +1,12 @@
-# from fastapi import FastAPI,UploadFile,File
-# from app.routes.health import router as health_router
+from fastapi import FastAPI
+from app.routes.health import router as health_router
 
-# app=FastAPI()
+app=FastAPI()
 
-# app.include_router(health_router)
+app.include_router(health_router)
 
-# @app.get("/health")
-# def check_health():
-#     return {
-#         "msg":"server is running and up"
-#     }
-    
-# @app.get("upload")    
-# async def upload(
-#     name:str,
-#     email:str,
-#     file:UploadFile=File(...,example="resume.pdf")
-#     ):
-    
-#     await content=file.read()
-#     resumes[file.filename]={
-#        " name":name,
-#          "email":email,
-#          "filename":file.filename,
-#          "file":file
-#     }
-#     return resumes
+@app.get("/health")
+def check_health():
+    return {
+        "msg":"server is running and up"
+    }
