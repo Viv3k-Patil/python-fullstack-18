e47e02f00c95153c1c79c3e0ee30824763da69f7
Akshata-Bhoite-Git <akshatabhoite0101@gmail.com>
2026-05-19 20:16:24 +0530

    add some code

--- projects/capstone-project/mockready/database/database.py
+++ projects/capstone-project/mockready/database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql://postgres:Akshu@0101@localhost:5432/mockready"
+DATABASE_URL = "postgresql://neondb_owner:npg_W3cyKHnXL9dl@ep-quiet-voice-aplrv8k0-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 engine = create_engine(DATABASE_URL)
 
