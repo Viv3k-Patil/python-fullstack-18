524d8ff3cd3ab5e0500879e47266a3564a45efdb
sameerawate2 <sameerawate2@gmail.com>
2026-05-21 01:36:11 +0530

    exception code clean

--- code/exception/exception.py
+++ code/exception/exception.py
@@ -5,14 +5,7 @@ print("something")
 print("something")
 print("something")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 8ee2b4665817a3550d1895555cb83836724637f7
-=======
->>>>>>> ea3141f4e13ba1afa5fb4513ad9ddaf7245c89d2
->>>>>>> 1cbf00331909a46a54aae8247e9731cb55397e45
+
 # try:
 #     int("abc")
 # except ValueError as e:
@@ -21,8 +14,7 @@ print("something")
 #     print("code works!!")
 # finally:
 #     print("cleaning..")
-<<<<<<< HEAD
-=======
+
 try:
     int("abc")
 except ValueError as e:
@@ -31,13 +23,7 @@ else:
     print("code works!!")
 finally:
     print("cleaning..")
->>>>>>> sameercode
-=======
-<<<<<<< HEAD
->>>>>>> 8ee2b4665817a3550d1895555cb83836724637f7
-=======
->>>>>>> ea3141f4e13ba1afa5fb4513ad9ddaf7245c89d2
->>>>>>> 1cbf00331909a46a54aae8247e9731cb55397e45
+
 
 
 # print("something")
