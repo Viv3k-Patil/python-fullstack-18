bbf72e16673f130116315fdf96ac022a18afdc3c
Akshata-Bhoite-Git <akshatabhoite0101@gmail.com>
2026-06-05 18:35:04 +0530

    Files

---
+++ team/akshata/Python_Batch18_New/react/my-app/src/Greet.jsx
@@ -0,0 +1,13 @@
+import GreetingDay from "./GreetingDay";
+
+
+function Greet(){
+    return(
+        <div>
+        <p>Hey there, I am a greet component!!</p>
+        <GreetingDay />
+        </div>
+    )
+}
+
+export default Greet;
