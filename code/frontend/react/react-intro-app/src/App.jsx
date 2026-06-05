4d73b5d775cfbdf53923df4ca332f8a5a66359d7
Viv3k-Patil <107409876+Viv3k-Patil@users.noreply.github.com>
2026-06-05 13:33:01 +0000

    revamp

---
+++ code/frontend/react/react-intro-app/src/App.jsx
@@ -0,0 +1,14 @@
+import ReactIntro from './ReactIntro';
+import Introduction from './Introduction';
+function App() {
+  return (
+    <div>
+      <ReactIntro />
+      <Introduction 
+        name="vivek patil"
+      />
+    </div>
+  )
+}
+
+export default App
