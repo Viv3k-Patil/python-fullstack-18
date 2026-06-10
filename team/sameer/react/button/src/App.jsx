64a7a0a5510fa35b1084549d55ebd134a2acbf93
sameerawate2 <sameerawate2@gmail.com>
2026-06-10 09:16:34 +0530

    new

--- team/sameer/react/button/src/App.jsx
+++ team/sameer/react/button/src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export default function App() {
+  function handleonClick(){
+    
+  }
+
+
+
   return (
-    <button className='button' onClick={handleonClick}>Click</button>
+    <button className='button' onClick={handleonClick}>Click me</button>
   )
 }
